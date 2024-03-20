574970062128baa0b88650038f13f45e9bdb5d00
Sergio <norgestream@gmail.com>
2024-03-20 13:59:09 +0200

    add timer and fix automaticaly change question when timer expires

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import QUESTIONS from '../questions';
 import quizCompleteImg from '../assets/quiz-complete.png';
+import QuestionTimer from './QuestionTimer';
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -9,9 +10,13 @@ export default function Quiz() {
 
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
   
-  function handleSelectAnswer(selectedAnswer) {
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
     setUserAnswers(prev => [...prev, selectedAnswer]);
-  }
+  }, []);
+
+  const handleSkipAnswer = useCallback(() => 
+    handleSelectAnswer(null), [handleSelectAnswer]
+  );
   
   if(quizIsComplete) {
     return (
@@ -21,13 +26,18 @@ export default function Quiz() {
       </div>
     )
   }
-  
+
   const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
   shuffledAnswers.sort(() => Math.random() - 0.5);
   
   return (
     <div id='quiz'>
       <div id='question'>
+        <QuestionTimer 
+          key={activeQuestionIndex}
+          timeout={10000} 
+          onTimeout={handleSkipAnswer}
+        />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id='answers'>
           {shuffledAnswers.map(answer => (
